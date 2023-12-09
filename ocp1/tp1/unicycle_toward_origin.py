4f3007c769ba4318e2aa8cab26a8092b9ff7acfa
Mansard <nmansard@laas.fr>
2023-12-09 12:47:05 +0100

    OCP1: adding bicopter to notebook 1 (partial)

--- ocp1/tp1/unicycle_toward_origin.py
+++ ocp1/tp1/unicycle_toward_origin.py
@@ -16,27 +16,34 @@ model = crocoddyl.ActionModelUnicycle()
 # %jupyter_snippet termmodel
 model_term = crocoddyl.ActionModelUnicycle()
 
-model_term.costWeights = np.matrix([
+model_term.costWeights = np.array([
     100,   # state weight
     0  # control weight
 ]).T
+# %end_jupyter_snippet
 
 # Define the optimal control problem.
 problem = crocoddyl.ShootingProblem(x0, [ model ] * T, model_term)
+# %jupyter_snippet ddp
 # Select the solver for this problem
 ddp = crocoddyl.SolverDDP(problem)
 # %end_jupyter_snippet
 
+# %jupyter_snippet callback
 # Add solvers for verbosity and plots
 ddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+# %end_jupyter_snippet
 
 ### SOLVE THE PROBLEM
 
+# %jupyter_snippet solve
 done = ddp.solve()
 assert(done)
+# %end_jupyter_snippet
 
 ### PLOT 
 
+# %jupyter_snippet plotlog
 log = ddp.getCallbacks()[0]
 crocoddyl.plotOCSolution(log.xs, log.us, figIndex=1, show=False)
 crocoddyl.plotConvergence(
@@ -49,6 +56,8 @@ crocoddyl.plotConvergence(
     figIndex=2,
     show=False,
 )
+# %end_jupyter_snippet
+
 plotUnicycleSolution(log.xs)
 
 print('Type plt.show() to display the result.')
