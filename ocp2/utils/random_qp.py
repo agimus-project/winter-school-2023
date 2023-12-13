489c6e5f8a2ff2ec09f13a647a454693932cb4d2
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-13 16:32:39 +0100

    commit now

--- ocp2/utils/random_qp.py
+++ ocp2/utils/random_qp.py
@@ -4,7 +4,7 @@ from scipy.stats import ortho_group
 from collections import namedtuple
 import unittest
 
-QP = namedtuple("QP", ["Q", "q", "A", "b"])
+QP = namedtuple("QP", ["Q", "A", "q", "b"])
 
 
 
@@ -22,13 +22,17 @@ def gaussian_orthogonal_ensemble(n):
     return 0.5 * (A + A.T)
 
 
-def assemble_kkt_system(Q, J, q, c):
+def assemble_kkt_system(qp: QP):
     """Assemble a KKT system (left-hand side and right-hand side)."""
-    nc = c.shape[0]
-    assert J.shape[0] == nc
-    A = np.block([[Q, J.T], [J, np.zeros(nc, nc)]])
-    r = np.concatenate([q, c])
-    return A, r
+    Q = qp.Q
+    q = qp.q
+    A = qp.A
+    b = qp.b
+    nc = b.shape[0]
+    assert A.shape[0] == nc
+    mat = np.block([[Q, A.T], [A, np.zeros(nc, nc)]])
+    r = np.concatenate([q, b])
+    return mat, r
 
 
 def generate_convex_eqp(n, p, nc, check_strictly_convex = False):
@@ -39,10 +43,47 @@ def generate_convex_eqp(n, p, nc, check_strictly_convex = False):
     if check_strictly_convex:
         assert n <= p
     Q = wishart(n, p)
-    J = np.random.randn(nc, n)
+    A = np.random.randn(nc, n)
 
     q = np.random.randn(n)
-    c = np.random.randn(nc)
+    b = np.random.randn(nc)
 
-    return [Q, J, q, c]
+    return QP(Q, A, q, b)
 
+
+def generate_convex_qp_nolicq(n, p, nc, nredundant=1):
+    """Generate a convex QP which lacks LICQ conditions, by making some row constraints redundant.
+    """
+
+    qp = generate_convex_eqp(n, p, nc, False)
+
+    # modify the A matrix to add redundant rows
+    ridx = list(np.random.choice(nc, nredundant))
+    print(qp.A.shape)
+    print(ridx)
+
+    rows = qp.A[ridx, :].copy()
+    if nredundant > 1:
+        random_rot = ortho_group.rvs(nredundant)
+        rows = random_rot @ rows
+    else:
+        rows = -rows
+    rows = rows.reshape(nredundant, n)
+    print(rows.shape)
+    qp.A = np.vstack([qp.A, rows])
+    assert qp.A.shape[0] == nc + nredundant
+
+    return qp
+
+
+
+class Test(unittest.TestCase):
+    def test_nolicq(self):
+        qp = generate_convex_qp_nolicq(4, 4, 3, 2)
+        kkt, _ = assemble_kkt_system(qp)
+        print(kkt)
+        self.assertAlmostEqual(npla.det(kkt), 0.)
+
+
+if __name__ == "__main__":
+    Test().test_nolicq()
