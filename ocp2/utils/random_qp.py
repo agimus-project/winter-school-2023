2b70131e411fbaaff2e2e27e8dc0cc67960b1ec3
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-13 16:38:10 +0100

    commit working test_nolicq

--- ocp2/utils/random_qp.py
+++ ocp2/utils/random_qp.py
@@ -30,7 +30,7 @@ def assemble_kkt_system(qp: QP):
     b = qp.b
     nc = b.shape[0]
     assert A.shape[0] == nc
-    mat = np.block([[Q, A.T], [A, np.zeros(nc, nc)]])
+    mat = np.block([[Q, A.T], [A, np.zeros((nc, nc))]])
     r = np.concatenate([q, b])
     return mat, r
 
@@ -59,30 +59,40 @@ def generate_convex_qp_nolicq(n, p, nc, nredundant=1):
 
     # modify the A matrix to add redundant rows
     ridx = list(np.random.choice(nc, nredundant))
-    print(qp.A.shape)
-    print(ridx)
 
     rows = qp.A[ridx, :].copy()
+    br = qp.b[ridx].copy()
     if nredundant > 1:
         random_rot = ortho_group.rvs(nredundant)
         rows = random_rot @ rows
+        br = random_rot @ br
     else:
         rows = -rows
+        br = -br
     rows = rows.reshape(nredundant, n)
-    print(rows.shape)
-    qp.A = np.vstack([qp.A, rows])
-    assert qp.A.shape[0] == nc + nredundant
+    Anew = np.vstack([qp.A, rows])
+    bnew = np.concatenate([qp.b, br])
 
-    return qp
+    assert Anew.shape[0] == nc + nredundant
+    assert bnew.shape[0] == Anew.shape[0]
+
+    return QP(qp.Q, Anew, qp.q, bnew)
 
 
 
 class Test(unittest.TestCase):
+    def test_strict_convex(self):
+        qp = generate_convex_eqp(4, 5, 2, True)
+        kkt, _ = assemble_kkt_system(qp)
+        d = npla.det(kkt)
+
     def test_nolicq(self):
         qp = generate_convex_qp_nolicq(4, 4, 3, 2)
         kkt, _ = assemble_kkt_system(qp)
         print(kkt)
-        self.assertAlmostEqual(npla.det(kkt), 0.)
+        d = npla.det(kkt)
+        print("det:", d)
+        self.assertAlmostEqual(d, 0.)
 
 
 if __name__ == "__main__":
